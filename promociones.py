46dbefc89d065d6b706f1fe40a7d1be7457c775d
Andres Garcia Burgos <andresgarcia.kaox@gmail.com>
2019-12-13 01:02:34 -0500

    .

--- promociones.py
+++ promociones.py
@@ -1,14 +1,54 @@
-from urllib.request import Request, urlopen
+import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-url = "https://www.python.org"
-#url = "https://www.adidas.pe"
+def getSoup(url, header):
+    response = requests.get(url, headers=header)
+    return BeautifulSoup(response.text, "html.parser")
 
-req = Request(url, headers = {"User-Agent": "Mozilla/5.0"})
+def getText(str):
+    item = ''
+    i = 0
+    while str[i] != '>':
+         i += 1
+    i += 1
+    while str[i] != '<':
+        item += str[i]
+        i+=1
+    return item
 
+def scrapeAdidas():
+    url = 'https://www.adidas.com/us/women-shoes'
+    header = {
+        'User-Agent': 'My User Agent 1.0', 
+        'From': 'youremail@domain.com'
+    } 
+    soup = getSoup(url, header)
 
-web_byte = urlopen(req).read()
-webpage = web_byte.decode('utf-8')
+    # browser = webdriver.Chrome()
+    # browser.get(url)
+    # nav = browser.find_element_by_id("gl-price__value")
+    # print(nav.text)
 
-res = BeautifulSoup(web_byte.decode(),"html.parser")
-print(webpage)
+    # print(soup.find_all("div", class_="gl-product-card__details-main"))
+    products = {}
+
+    for div in soup.find_all("div", class_="gl-product-card__details-main"):
+        print(div.text)
+        # for name in div.find_all("div", class_="gl-product-card__name gl-label gl-label--m"):
+        #     print("Name:" + name.text)
+        # for price in div.find_all("div", class_="gl-price gl-price--s gl-price__inline___-VD1g"):
+        #     print("Price:" + price.text)
+
+    for price in soup.find_all("span", class_="gl-price__value"):
+        print(price)
+
+    count = 1
+    for i in soup.findAll('title'):
+        products['Adidas '+ str(count)] = getText(str(i))
+        count += 1
+    return products
+
+yeezyProducts = scrapeAdidas()
+
+# print (yeezyProducts)
